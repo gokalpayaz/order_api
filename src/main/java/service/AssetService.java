0345eb0653990f8cf39bf41723475883d1db3730
Gökalp Ayaz <gokalpayaz@windowslive.com>
2025-05-08 00:12:15 +0300

    AssetService - listAssets added

---
+++ src/main/java/service/AssetService.java
@@ -0,0 +1,19 @@
+package service;
+
+import com.brokerage.order_api.model.Asset;
+import com.brokerage.order_api.model.Customer;
+import com.brokerage.order_api.repository.AssetRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AssetService {
+    private final AssetRepository assetRepository;
+
+    public List<Asset> listAssets(Customer customer){
+        return assetRepository.findByCustomerId(customer.getId());
+    }
+}
