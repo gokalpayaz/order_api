8f89c30c6d75cd27d576f777903f59847f80258d
Gökalp Ayaz <gokalpayaz@windowslive.com>
2025-05-09 15:52:35 +0300

    New exceptions defined

---
+++ src/main/java/com/brokerage/order_api/exception/InsufficientFundsException.java
@@ -0,0 +1,9 @@
+package com.brokerage.order_api.exception;
+
+public class InsufficientFundsException extends RuntimeException {
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
+
+
